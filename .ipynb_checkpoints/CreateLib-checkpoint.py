1b175cfc196c6017a92a953b0976a9d3d592a464
chrisbuerginrogers <crogers@tufts.edu>
2022-08-24 08:23:38 -0400

    Added CreateLib Editing
    also fixed abort() and changed the order of methods in CreateLib

--- .ipynb_checkpoints/CreateLib-checkpoint.py
+++ .ipynb_checkpoints/CreateLib-checkpoint.py
@@ -21,7 +21,58 @@ class Create():
         self.audio_publisher = Audio(namespace)
         self.serial = None
         time.sleep(1)
+
+    def LED(self,color):
+        '''
+        changes the color of the LED
+        '''
+        led_colors = color
+        print('publish LED ', end = ''        )
+        self.led_publisher.set_color(led_colors)
+        time.sleep(1)
+        print('done')
+
+    def beep(self, frequency = 440):
+        '''
+        Beeps
+        '''
+        print('publish beep ', end = ''        )
+        self.audio_publisher.beep(frequency)
+        time.sleep(1)
+        print('done')
+            
+    def turn(self,angle = 90):
+        '''
+        rotates a given angle
+        '''
+        speed = 0.5   
+        angle = angle/180*3.1415
+        print('turn %0.2f: goal' % angle, end = '')
+        self.rotate_client.set_goal(float(angle), speed)
+        print(' set ', end = '')
+        self.wait(self.rotate_client)
+        print('done')
+
+    def forward(self,dist = 0.5):
+        '''
+        goes the distance and then stops the ROS2 connection
+        '''
+        speed = 0.25
+        print('forward %0.2f: goal' % dist, end = '')
+        self.drive_client.set_goal(float(dist),speed)
+        print(' set ', end = '')
+        self.wait(self.drive_client)
+        print('done')
         
+    def wait(self, client):
+        rclpy.spin_once(client)
+        while not client.done:
+            #time.sleep(0.1)
+            print('...', end = '')
+            rclpy.spin_once(client)
+
+# ----------------------------------------serial calls using serial over TCP------------------------- 
+
     def serial_init(self, IP, PORT, timeout = 0):
         self.serial = TCPServer (IP, PORT, timeout)
         
@@ -37,7 +88,7 @@ class Create():
         else:
             print('serial not initialized')
             
-    def serial_abort():
+    def serial_abort(self):
         self.serial.write_binary(b'\x03')
             
     def serial_run(self, code):
@@ -62,55 +113,6 @@ class Create():
             return self.serial.close()
         else:
             print('serial not initialized')
-        
-    def wait(self, client):
-        rclpy.spin_once(client)
-        while not client.done:
-            #time.sleep(0.1)
-            print('...', end = '')
-            rclpy.spin_once(client)
-
-    def forward(self,dist = 0.5):
-        '''
-        goes the distance and then stops the ROS2 connection
-        '''
-        speed = 0.25
-        print('forward %0.2f: goal' % dist, end = '')
-        self.drive_client.set_goal(float(dist),speed)
-        print(' set ', end = '')
-        self.wait(self.drive_client)
-        print('done')
-    
-    def turn(self,angle = 90):
-        '''
-        rotates a given angle
-        '''
-        speed = 0.5   
-        angle = angle/180*3.1415
-        print('turn %0.2f: goal' % angle, end = '')
-        self.rotate_client.set_goal(float(angle), speed)
-        print(' set ', end = '')
-        self.wait(self.rotate_client)
-        print('done')
-    
-    def LED(self,color):
-        '''
-        changes the color of the LED
-        '''
-        led_colors = color
-        print('publish LED ', end = ''        )
-        self.led_publisher.set_color(led_colors)
-        time.sleep(1)
-        print('done')
-
-    def beep(self, frequency = 440):
-        '''
-        Beeps
-        '''
-        print('publish beep ', end = ''        )
-        self.audio_publisher.beep(frequency)
-        time.sleep(1)
-        print('done')
 
     def close(self):
         self.drive_client.destroy_node()
